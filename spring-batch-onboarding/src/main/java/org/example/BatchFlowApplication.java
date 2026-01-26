3bb785a4073fa0836fd3bdabb44454867a43e7f6
corollazero <wovk1010@gmail.com>
2026-01-26 22:13:11 +0900

    "feat: Initial batch Application"

--- spring-batch-onboarding/src/main/java/org/example/BatchFlowApplication.java
+++ spring-batch-onboarding/src/main/java/org/example/BatchFlowApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BatchFlowApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BatchApplication.class, args);
+        SpringApplication.run(BatchFlowApplication.class, args);
     }
 }
